647aba60f3f827adb114e2477ffab017df1b5c7e
jidefakoya <jidefakoya@hotmail.co.uk>
2020-04-24 11:12:35 +0100

    Add future date request test

--- src/test/java/com/hsbc/gcmb/utils/APIPaths.java
+++ src/test/java/com/hsbc/gcmb/utils/APIPaths.java
@@ -9,6 +9,7 @@ public enum APIPaths {
     LATEST_RATES_PATH_WITH_BASE_CURRENCY("/api/latest/?base=%s"),
     LATEST_RATES_PATH_WITH_BASE_CURRENCY_AND_SYMBOLS("/api/latest/?base=%s&symbols=%s"),
     HISTORICAL_RATES_PATH("/api/%s"),
+    FUTURE_RATES_PATH("/api/%s"),
     HISTORICAL_RATES_PATH_WITH_SYMBOLS("/api/%s?symbols=%s"),
     HISTORICAL_RATES_PATH_WITH_BASE_CURRENCY("/api/%s?base=%s"),
     HISTORICAL_RATES_PATH_WITH_BASE_CURRENCY_AND_SYMBOLS("/api/%s?base=%s&symbols=%s");
